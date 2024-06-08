f6bbae639d1f9eb2f02c224105740f903d736710
NguyenQuocKhoi <nguyenquockhoi29122002@gmail.com>
2024-06-08 16:14:43 +0700

    customize axios

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,5 +1,6 @@
-import axios from "axios";
+// import axios from "axios";
+import axios from "./axios";
 
 export const fetchAllUser = () => {
-  return axios.get("https://reqres.in/api/users?page=1");
+  return axios.get("api/users?page=1");
 };
